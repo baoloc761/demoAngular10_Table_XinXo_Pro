600fa06fa219f890610a99b318681293f5d3416f
Phan Minh Dat <dat.phan.minh@mediastep.com>
2023-11-28 14:35:13 +0700

    Apply routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,60 +1,9 @@
 import { Component } from "@angular/core";
-import * as _ from "lodash";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  styles: ["./app.component.css"],
 })
 export class AppComponent {
-  title = "Danh sach mat hang q4";
-  thList = [
-    { value: "San luong", css: "col-1" },
-    { value: "Ten mat hang", css: "col-2" },
-    { value: "Khau hao", css: "col-3" },
-    { value: "Status", css: "col-4" },
-    { value: "%", css: "col-5" },
-  ];
-
-  body = [
-    {
-      css: "tr-1",
-      sort: 0,
-      tdList: [
-        { value: 125575000.899, css: "col-1", isAccounting: true },
-        { value: "abc", css: "col-2" },
-        { value: 0.1, css: "col-3" },
-        { value: 1, css: "col-4 ch", type: "bool" },
-        { value: 75, css: "col-5", isProgressBar: true },
-      ],
-    },
-    {
-      css: "tr-2",
-      sort: 3,
-      tdList: [
-        { value: 75950.175, css: "col-1", isAccounting: true },
-        { value: "xyz", css: "col-2" },
-        { value: 0.17, css: "col-3" },
-        { value: 0, css: "col-4 hh", type: "bool" },
-        { value: 11, css: "col-5", isProgressBar: true },
-      ],
-    },
-    {
-      css: "tr-3",
-      sort: 2,
-      tdList: [
-        { value: 215000.7, css: "col-1", isAccounting: true },
-        { value: "hh", css: "col-2" },
-        { value: 0.17, css: "col-3" },
-        { value: 1, css: "col-4 ch", type: "bool" },
-        { value: 92, css: "col-5", isProgressBar: true },
-      ],
-    },
-  ];
-
-  sortedBody = this.sortBy(this.body, "sort", "desc");
-
-  sortBy(list: any[], prop: string, direction: string) {
-    return _.sortBy(list, [prop], [direction]);
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,29 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
 
+import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { DetailsComponent } from "./details/details.component";
+import { HomeComponent } from "./home/home.component";
+import { HousingLocationComponent } from "./housing-location/housing-location.component";
 import { MyTableComponent } from "./mytable/mytable.component";
 
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
-  declarations: [AppComponent, MyTableComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    MyTableComponent,
+    HomeComponent,
+    DetailsComponent,
+    HousingLocationComponent
+  ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
